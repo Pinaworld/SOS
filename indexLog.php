bea699159181e4e47ae0fb80539b85227a205870
Bemp <Pinben02@gmail.com>
2015-09-11 09:44:49 +0200

    Css Fondation

---
+++ indexLog.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benoit
+ * Date: 11/09/2015
+ * Time: 09:39
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.css">
+
+    <link rel="stylesheet" href="css/foundation.css">
+
+    <!-- This is how you would link your custom stylesheet -->
+    <link rel="stylesheet" href="css/foundation.css">
+
+    <link rel="stylesheet" href="CSSindex.css">
+
+
+    <script src="js/vendor/modernizr.js"></script>
+
+    <title>Accueil - SOS Partenaires</title>
+
+</head>
+<body>
+
+<div class="row">
+    <div class="large-3 columns">
+        <h1><img src="http://placehold.it/400x100&text=Logo"/></h1>
+    </div>
+    <div class="large-9 columns">
+        <ul class="right button-group">
+            <li><a href="#" class="button">Partenaires disponibles</a></li>
+            <li><a href="#" class="button">Mes Sports</a></li>
+            <li><a href="#" class="button">Mon compte</a></li>
+            <li><a href="#" class="button">Déconnexion</a></li>
+        </ul>
+    </div>
+</div>
+
+
+
+
+
+<div class="row">
+    <div class="large-12 columns">
+        <div id="slider">
+            <img src=img/sport1.jpg>
+        </div>
+
+        <hr/>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>This is a content section.</h4>
+        <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="large-12 columns">
+
+        <div class="panel">
+            <h4>Get in touch!</h4>
+
+            <div class="row">
+                <div class="large-9 columns">
+                    <p>We'd love to hear from you, you attractive person you.</p>
+                </div>
+                <div class="large-3 columns">
+                    <a href="#" class="radius button right">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<footer class="row">
+    <div class="large-12 columns">
+        <hr/>
+        <div class="row">
+            <div class="large-6 columns">
+                <p>© Copyright, but fictif {BTS}</p>
+            </div>
+            <div class="large-6 columns">
+                <ul class="inline-list right">
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,32 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Younes
- * Date: 07/09/2015
- * Time: 15:28
+ * User: Benoit
+ * Date: 11/09/2015
+ * Time: 09:32
  */
 
-
-$auth = 0;
-include 'lib/includes.php';
-/**
- *  TRAITEMENT DU FORMULAIRE
- */
-if(isset($_POST['username']) && isset ($_POST['password'])){
-    $password = sha1($_POST['password']);
-    $username = $db->quote($_POST['username']);
-    $select = $db->query("SELECT * FROM users WHERE username=$username AND password ='$password'");
-    if($select->rowCount() > 0){
-        $_SESSION['Auth']= $select->fetch();
-        setFlash('Vous êtes connecté');
-        header('location:' . WEBROOT . '/' . 'testconnection.php');
-        die();
-    }else header('location:'. WEBROOT . '/' . 'index.php');
-
-}
-/**
- * INCLUSION HEADER
- */
-
-
+    echo "Weee";
 ?>
